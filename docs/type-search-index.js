64d5f729aeacef4a25743e302812abdee05cf65c
rina-reimer <rinareimer616@gmail.com>
2024-02-12 15:44:36 -0800

    returned javadoc

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"bit.connect","l":"App"},{"p":"bit.connect","l":"Cloud"},{"p":"bit.connect","l":"Filter"},{"p":"bit.connect","l":"Grid"},{"p":"bit.connect","l":"Student"}];updateSearchResults();
