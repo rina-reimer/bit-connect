f0713e7d4ff707924e8d391da7731dcf91fbd1dc
rina-reimer <rinareimer616@gmail.com>
2024-02-11 22:11:26 -0800

    gradle build

--- README.md
+++ README.md
@@ -12,3 +12,11 @@ extended implementation of WINFO Hackathon project
 
 bounds for latitude: (47°39'04.4"N, 47°39'40.1"N )
 bounds for longitude: (122°18'43.5"W, 122°18'03.6"W)
+
+
+- in terms of code readability, there's code quality
+- diagram is good
+- supplement with documentation - javadoc comments
+- guarantees/invariants
+- think about audience
+- multiple versions
--- java/Grid.java
+++ java/Grid.java
@@ -1,7 +1,8 @@
-package java;
+package javafiles;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +11,7 @@ public class Grid {
   private double[] latBounds;
   private double[] longBounds;
   private List<Student> currUsers;
-  private Map<Student, List<Student>> userMatch;
+  private Map<Student, LinkedList<Student>> userMatch;
 
   public Grid(int id, double lowerLat, double higherLat, double lowerLong, double higherLong) {
     this.id = id;
@@ -21,11 +22,37 @@ public class Grid {
   }
 
   public void refresh() {
+    this.currUsers.clear();
+    this.userMatch.clear();
     for (Student s : Cloud.activeUsers) {
-      // check latitude
+      // check latitude and longitude and add to matching map and list
+      if ((s.getCurrLoc()[0] > latBounds[0] && s.getCurrLoc()[0] < latBounds[1]) &&
+          (s.getCurrLoc()[1] > longBounds[0] && s.getCurrLoc()[1] < longBounds[1])) {
+        this.currUsers.add(s);
+        this.userMatch.put(s, new LinkedList<>());
+      }
+    }
 
-      // check longitude
+    for (int i = 0; i < currUsers.size(); i++) {
+      for (int j = i+1; j < currUsers.size(); j++){
+        if (currUsers.get(i).matchFilters(currUsers.get(j))) {
+          insert(currUsers.get(i), currUsers.get(j));
+          insert(currUsers.get(j), currUsers.get(i));
+        }
+      }
     }
   }
+
+  private void insert(Student origin, Student added) {
+    double dist = origin.distance(added);
+    LinkedList<Student> lst = this.userMatch.get(origin);
+    int index = 0;
+    while (lst.get(index+1).distance(origin) < dist) {
+      index++;
+    }
+    lst.add(index, added);
+  }
+
+  public int getId() { return id; }
 
 }
