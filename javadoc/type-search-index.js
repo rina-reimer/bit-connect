782f64b8164fe4648eaa92f26b01fe9370547969
rina-reimer <102627869+rina-reimer@users.noreply.github.com>
2024-02-18 00:13:39 +0000

    Deploying to app/build/docs/javadoc from @ rina-reimer/bit-connect@0e62264bcc057c18b43e41b724724b2a2fc3f965 🚀

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"bit.connect","l":"App"},{"p":"bit.connect","l":"Cloud"},{"p":"bit.connect","l":"Filter"},{"p":"bit.connect","l":"Grid"},{"p":"bit.connect","l":"Student"}];updateSearchResults();
